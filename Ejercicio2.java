8bfc5061ff9acc97475560d546526d2164978625
PachecoASIR <jferradaspacheco@danielcastelao.org>
2022-12-16 20:11:20 +0100

    Commit5

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -1,39 +1,20 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Ejercicio2 {
 
-    public static void main(String[] args) {
- 
-        Scanner sc = new Scanner(System.in);
-        ArrayList<Integer> numeros = new ArrayList();
-        int n;
-  
-        do {
-            System.out.println("Introduce números enteros. 0 para acabar: ");                                     
-            System.out.println("Numero: ");
-            n = sc.nextInt();
-            if (n != 0){
-                numeros.add(n);
-            }
-        }while (n != 0);
-       
-        System.out.println("Ha introducido: " + numeros.size() + " números:");
-       
-        //mostrar el arrayList completo
-        System.out.println(numeros);
-       
-        //recorrido usando un iterador para mostrar un elemento por línea                                         
-        Iterator it = numeros.iterator();
-        while(it.hasNext()){
-              System.out.println(it.next());
-        }
-  
-        //recorrido usando foreach para sumar los elementos
-        double suma = 0;
-        for(Integer i: numeros){
-            suma = suma + i;
-        }
-        System.out.println("Suma: " + suma);
-        System.out.println("Media: " + suma/numeros.size());
+	public static int rand(int min , int max) {
+		return (int)Math.floor(Math.random()*(max-min+1)+min);	
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner( System.in );
+		
+		  System.out.println( "Esto es un generador de números random, a continuacción se le pedirá el rango en el que desee que busque el número." );
+		  System.out.println( "Introduce un número para el mínimo: " );
+		  int n1 = sc.nextInt();
+		  System.out.println( "Introduce un número para el maximo: " );
+		  int n2 = sc.nextInt();	
+		  System.out.println(rand(n1,n2));
     }
 }
