d5e9a033630bc1c40a48d176240fddb8d7a5fa56
PachecoASIR <jferradaspacheco@danielcastelao.org>
2022-12-18 19:45:11 +0100

    Commit6

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -13,8 +13,9 @@ public class Ejercicio2 {
 		  System.out.println( "Esto es un generador de números random, a continuacción se le pedirá el rango en el que desee que busque el número." );
 		  System.out.println( "Introduce un número para el mínimo: " );
 		  int n1 = sc.nextInt();
-		  System.out.println( "Introduce un número para el maximo: " );
+		  System.out.println( "Introduce un número para el máximo: " );
 		  int n2 = sc.nextInt();	
+		  System.out.println( "Tu número es: " );
 		  System.out.println(rand(n1,n2));
     }
 }
